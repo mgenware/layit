7b3284c122d8f92339497db9ea8e09a68573449b
Mgen <m@mgenware.com>
2018-03-26 03:30:06 +0800

    Update main.test.ts

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -4,6 +4,6 @@ import TestHandler from './testHandler';
 test('Test handler: plain text child', () => {
   const handler = new TestHandler();
   const builder = new Builder(handler);
-  const element = Builder.elementFromXML('<h>a &lt;</h>');
-  expect(builder.build(element)).toEqual([{v: 'a <'}]);
+  const element = Builder.elementFromXML('<h><view></view></h>');
+  expect(builder.build(element)).toEqual([{v: [{View: {}}]}]);
 });
