a30d4575f835a1a4e4d14243daa26f354993a838
Mgen <m@mgenware.com>
2018-04-01 22:56:41 +0800

    Export DOMParser

--- lib/main.ts
+++ lib/main.ts
@@ -2,3 +2,4 @@ export { Builder } from './builder';
 export { default as Handler } from './handler';
 export { default as Context } from './context';
 export { default as Defs } from './defs';
+export { DOMParser } from 'xmldom';
