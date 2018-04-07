516660601c9a8ed4e8463317e14e3d9dbf79892b
Mgen <m@mgenware.com>
2018-04-07 19:12:10 +0800

    Make defs uppercase

--- lib/defs.ts
+++ lib/defs.ts
@@ -1,8 +1,8 @@
 export class Defs {
   readonly v = 'V';
   readonly h = 'H';
-  readonly box = 'Box';
-  readonly layit = 'layit';
+  readonly box = 'BOX';
+  readonly layit = 'LAYIT';
 }
 
 export default new Defs();
