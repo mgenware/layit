10d74d05833f34b946861d71cd008c4970ee43e4
Mgen <m@mgenware.com>
2018-04-08 22:35:27 +0800

    Add box/v/h attributes to defs

--- lib/defs.ts
+++ lib/defs.ts
@@ -3,6 +3,10 @@ export class Defs {
   readonly h = 'H';
   readonly box = 'BOX';
   readonly layit = 'LAYIT';
+  readonly vhSize = 'size';
+  readonly boxMargin = 'margin';
+  readonly boxVAlign = 'v-align';
+  readonly boxHAlign = 'h-align';
 }
 
 export default new Defs();
